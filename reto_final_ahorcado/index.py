c4e5cd662e06daccc54a90178950c108a84c327d
FDamianRomagnoli <fdamianromagnoli@gmail.com>
2022-08-29 03:06:14 -0300

    python intermedio

--- reto_final_ahorcado/index.py
+++ reto_final_ahorcado/index.py
@@ -1,21 +1,51 @@
+from multiprocessing.resource_sharer import stop
 import random
 import os
 
 def main():
-    palabra = getWord()
-    printMenu(palabra)
-    palabra_a_completar = getIncompleted(palabra)
-    print(palabra_a_completar)
+    palabra = list(getWord())
+    palabra_a_completar = list(getIncompleted(palabra))
     juego_terminado = False
+    printMenu(palabra_a_completar)
 
     while juego_terminado == False:
-        printMenu
+        letra = getCharacter()
+        palabra_a_completar = compare(palabra,palabra_a_completar,letra)
+        printMenu(palabra_a_completar)
+        juego_terminado = isWin(palabra,palabra_a_completar)
+    
+    print("Felicidades, GANASTE")
+
+
+def isWin(palabra, palabraJugador):
+    return ''.join(palabra) == ''.join(palabraJugador)
+
+
+def getCharacter():
+    try:
+        letra = input('Ingrese un caracter: ')
+        assert len(letra) == 1,'Ingrese un caracter..'
+        assert letra.isnumeric() == False, 'Ingrese una letra'
+    except ValueError:
+        print(ValueError)
+
+    return letra
+
+def compare(palabra_completo,palabra_incompleta,letra):
+
+    palabra_final = ""
+
+    for (i,c) in enumerate(palabra_completo,0):
+        if palabra_completo[i] == letra:
+            palabra_final = palabra_final + letra
+        else:
+            palabra_final = palabra_final + palabra_incompleta[i]
+
+    return palabra_final
+
 
 def getIncompleted(palabra):
-    incompleted = ""
-    for l in palabra:
-        incompleted = incompleted + "_ "
-    return incompleted
+    return list(map(lambda a: '_',palabra))
 
 def getWord():
     with open('reto_final_ahorcado/data.txt','r', encoding='utf-8') as f:
@@ -26,14 +56,18 @@ def getWord():
         for line in f:
             contador = contador + 1
             if(contador == numero_random):
-                palabra = line
+                palabra = line.strip()
 
     return palabra
         
 
 def printMenu(text):
     os.system('clear')
-    print(f'!Adivine la palabra!\n\t{text}')
+    print('!Adivine la palabra!\n\t')
+    for l in text:
+        print(f'{l.upper()}', end=" ")
+    print('\n\t')
+
 
 
 
